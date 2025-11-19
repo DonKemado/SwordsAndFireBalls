f32be532b21de6af8bb4fb5542610fa7ba43dbd8
Beja <benkrat78@gmail.com>
2025-11-19 21:29:48 +0100

    Mas metodos y correccion de sintaxis

--- juego.js
+++ juego.js
@@ -11,15 +11,18 @@ class Juego {
         this.ejercitoEnemigo = [];
     }
 
-s    //Introduce el modo a traves de int
-    elegirDificultad(modo) {
+    //Introduce el modo a traves de int
+    elegirDificultad() {
+        let modo;
         do {
+            modo = parseInt(prompt("Elige dificultad:\n1. Fácil (2 rondas)\n2. Difícil (4 rondas)"));
+
             if (modo === 1) {
                 this.rondas = 2;
             } else if (modo === 2) {
                 this.rondas = 4;
             } else {
-                alert("Modo de dificultad no válido.");
+                alert("Modo no válido. Elige 1 o 2.");
             }
         } while (modo !== 1 && modo !== 2);
     }
@@ -40,18 +43,96 @@ s    //Introduce el modo a traves de int
         menuTexto += opciones.join("\n");
 
         const eleccion = prompt(menuTexto + "\n\nSeleccione una opcion (1-6):");
+        const opcionNumero = parseInt(eleccion);
 
-        //SWITCH para las opciones
-        return parseInt(eleccion);
+        //Opciones
+        switch (opcionNumero) {
+            case 1:
+                this.gestionarContratacion();
+                break;
+            case 2:
+                this.despedirUnidad();
+                break;
+            case 3:
+                this.combatir();
+                break;
+            case 4:
+                this.recuperarUnidades();
+                break;
+            case 5:
+                this.verEstado();
+                break;
+            case 6:
+                //Creo que se hace más fácil con un confirm
+                let salir = prompt("Estas seguro de que quieres salir?");
+                if (salir === 'si') {
+                    return 'salir';
+                } else {
+                    return 'continuar';
+                }
+                break;
+            default:
+                alert("Opcion no valida, elige entre el 1 - 6");
+        }
+        return 'continuar';
     }
-    //Si devuelve true, el juego termina
+
+    //Texto sencillito que junta caracteristicas de las unidades del jugador,
+    //y las junta en estadoTexto para devolverlas por el swich del menu principal
+    verEstado() {
+        let estadoTexto = "***ESTADO***\n";
+        estadoTexto += `Oro: ${this.oro} | Unidades: ${this.ejercitoJugador.length}/5\n\n`;
+        if (this.ejercitoJugador.length === 0) {
+            estadoTexto += "No tienes unidades en tu ejercito\n";
+        } else {
+            for (let i = 0; i < this.ejercitoJugador.length; i++) {
+                const unidad = this.ejercitoJugador[i];
+                estadoTexto += `${i + 1}. ${unidad.tipo} | Vida: ${unidad.puntosVida} | Ataque: ${unidad.poderAtaque}\n`;
+            }
+        }
+        alert(estadoTexto);
+    }
+
+    recuperarUnidades() {
+        //Comprobar si hay unidades de para recuperar
+        if (this.ejercitoJugador.length === 0) {
+            alert("No hay unidades en tu equipo");
+            return;
+        }
+        //Comprobar si recuperacion disponible
+        if (!this.recuperacionDisponible) {
+            alert("Recuperacion no disponible");
+            return;
+        }
+        //Ahora si, recuperar unidades:
+        for (let i = 0; i < this.ejercitoJugador.length; i++) {
+            const unidad = this.ejercitoJugador[i];
+            //70% de la vida maxima añadida
+            const vidaRecuperada = Math.floor(unidad.puntosVidaMaximos * 0.7);
+            unidad.puntosVida += vidaRecuperada;
+            //Por si acaso excede la vida máxima
+            if (unidad.puntosVida > unidad.puntosVidaMaximos) {
+                unidad.puntosVida = unidad.puntosVidaMaximos;
+            }
+            //Recargar habilidades
+            unidad.usosHabilidad = unidad.usosMaximos;
+            if (unidad.tipo === 'Mago') {
+                unidad.bolaFuegoUsada = false;
+            }
+        }
+        //Desactivamos la recuperacion
+        this.recuperacionDisponible = false;
+
+        alert("Unidades recuperadas!");
+    }
+
+    //Verificacion de que el juego termina
     verificarFin() {
         if (this.rondasJugadas >= this.rondas) {
             return 'ganado';
-        }else if (this.derrotasNow >= this.derrotasMax) {
+        } else if (this.derrotasNow >= this.derrotasMax) {
             return 'perdido';
         }
+        return 'continuar';
     }
 }
-const juego = new Juego();
-juego.mostrarMenu();
