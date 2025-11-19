bf47c3420d826597df0ee5c3c4a393f593dd2075
Beja <132787179+DonKemado@users.noreply.github.com>
2025-11-19 18:52:21 +0100

    Vamos avanzando

--- juego.js
+++ juego.js
@@ -4,19 +4,13 @@ class Juego {
         this.rondasJugadas = 0;
         this.derrotasMax = 2;
         this.derrotasNow = 0;
-        this.oro = 0; //Nos tocará elegir la cantidad incial
+        this.oro = 4000; //Nos tocará elegir la cantidad incial
         this.intentosContratacion = 6;
         this.recuperacionDisponible = false;
         this.ejercitoJugador = [];
         this.ejercitoEnemigo = [];
     }
-    verificarFin() {
-        if (this.rondasJugadas >= this.rondas) {
-            return true; //El juego ha terminado
-        }else if (this.derrotasNow >= this.derrotasMax) {
-            return true; //El juego ha terminado
-        }
-    }
+
 s    //Introduce el modo a traves de int
     elegirDificultad(modo) {
         do {
@@ -29,7 +23,35 @@ s    //Introduce el modo a traves de int
             }
         } while (modo !== 1 && modo !== 2);
     }
-    mostarMenu() {
+    mostrarMenu() {
+        const opciones = [
+            "1. Contratar unidades",
+            "2. Despedir unidades",
+            "3. Atacar",
+            "4. Recuperarse",
+            "5.Ver estado de las unidades",
+            "6. Salir del juego"
+        ];
+        //Lo que enseña nuestro menu principal
+        let menuTexto = "***MENU***\n";
+        menuTexto += `Rondas: ${this.rondasJugadas}/${this.rondas} | Derrotas: ${this.derrotasNow}/${this.derrotasMax}\n`;
+        menuTexto += `Oro: ${this.oro} | Ejercito: ${this.ejercitoJugador.length}/5\n`;
+        menuTexto += `Intentos de contratación: ${this.intentosContratacion}\n\n`;
+        menuTexto += opciones.join("\n");
 
+        const eleccion = prompt(menuTexto + "\n\nSeleccione una opcion (1-6):");
+
+        //SWITCH para las opciones
+        return parseInt(eleccion);
+    }
+    //Si devuelve true, el juego termina
+    verificarFin() {
+        if (this.rondasJugadas >= this.rondas) {
+            return 'ganado';
+        }else if (this.derrotasNow >= this.derrotasMax) {
+            return 'perdido';
+        }
     }
 }
+const juego = new Juego();
+juego.mostrarMenu();
